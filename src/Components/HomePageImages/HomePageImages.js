f3bbfcab24a032876240576bde746ce7a0662c8f
Naa Norkor <nortey.sarah@gmail.com>
2022-10-27 12:14:52 +0100

    initial setup

---
+++ src/Components/HomePageImages/HomePageImages.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import broccoli from "../../assets/images/broccoli.jpeg";
+
+
+
+function HomePageImages() {
+  return (
+    <div>
+        <img src={broccoli} alt="broccoli" />
+
+        <h1>hi</h1>
+    </div>
+  )
+}
+
+export default HomePageImages;
---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomePageImages from '../../Components/HomePageImages/HomePageImages'
+
+export default function HomePage() {
+  return (
+    <div>
+        <HomePageImages/>
+
+    </div>
+  )
+}
