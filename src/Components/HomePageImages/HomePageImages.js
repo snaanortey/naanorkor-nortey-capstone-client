93504f96a2b978c7b9e43a93b9770084436222c8
Naa Norkor <nortey.sarah@gmail.com>
2022-10-28 22:55:07 +0100

    fonts setup, creates homepage component

--- src/Components/HomePageImages/HomePageImages.js
+++ src/Components/HomePageImages/HomePageImages.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import broccoli from "../../assets/images/broccoli.jpeg";
+import "./HomePageImages.scss";
+// import broccoli from "../../assets/images/broccoli.jpeg";
 
 
 
 function HomePageImages() {
   return (
     <div>
-        <img src={broccoli} alt="broccoli" />
+        
 
         <h1>hi</h1>
     </div>
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,11 +1,42 @@
-import React from 'react'
-import HomePageImages from '../../Components/HomePageImages/HomePageImages'
+import React from "react";
+import HomePageImages from "../../Components/HomePageImages/HomePageImages";
+import axios from "axios";
+import { useState } from "react";
+import "./HomePage.scss";
 
 export default function HomePage() {
+  const [suggestedmeal, setSuggestedMeal] = useState([]);
+
+  const getSuggestedMeal = (e) => {
+    const value = e.target.value;
+
+    if (value.length >= 3) {
+      axios.get(`http://localhost:8000/suggest/${value}`).then((res) => {
+        setSuggestedMeal(res.data);
+      });
+    }
+    if (value.length <= 2) {
+      setSuggestedMeal([]);
+    }
+  };
+
   return (
     <div>
-        <HomePageImages/>
+      <HomePageImages />
+      <div className="first">
+        <input
+          onChange={getSuggestedMeal}
+          type="text"
+          placeholder="type your meal"
+          name="search"
+        />
+      </div>
 
+      <div>
+        {suggestedmeal.map((eachmeal, i) => (
+          <p key={i}>{eachmeal}</p>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
