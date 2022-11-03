6bec860d5aa200a7978a56bc66df50cfbcafd796
Naa Norkor <nortey.sarah@gmail.com>
2022-11-03 03:55:39 +0000

    feature/getRecipes page

--- src/Pages/GetRecipes/GetRecipes.js
+++ src/Pages/GetRecipes/GetRecipes.js
@@ -2,17 +2,19 @@ import axios from "axios";
 import React from "react";
 import { useState } from "react";
 
-export default function IngredientList() {
+export default function GetRecipes() {
   const [ingredientList, getIngredientList] = useState([]);
 
   const ingredientsBoxHandler = (e) => {
     e.preventDefault();
     const ingredientBox = e.target.ingredients.value;
     const ingredientBoxInArray = ingredientBox.split("\n");
+    console.log(ingredientBoxInArray);
 
     axios
-      .get("http://localhost:8000/ingredientlist", { ingredientBoxInArray })
+      .post("http://localhost:8000/recipes/search", ingredientBoxInArray)
       .then((res) => {
+        console.log(res.data);
         getIngredientList(res.data);
       });
 
@@ -29,9 +31,9 @@ export default function IngredientList() {
         {ingredientList.map((ingredient, i) => (
           <div key={i}>
             <div>
-              <h3>{ingredient.title}</h3>
+              <h3>{ingredient._source.title}</h3>
             </div>
-            <div>{ingredient.instructions}</div>
+            <div>{ingredient._source.instructions}</div>
           </div>
         ))}
       </div>
