3c363d02e0918d35e9a6c1c4e7136bc49d1293fe
Naa Norkor <nortey.sarah@gmail.com>
2022-11-04 03:10:14 +0000

    homepage styling

--- src/Components/HomePageImages/HomePageImages.js
+++ src/Components/HomePageImages/HomePageImages.js
@@ -1,32 +1,14 @@
 import React from "react";
 import "./HomePageImages.scss";
-import broccoli from "../../assets/images/broccoli.jpeg";
-import carrot from "../../assets/images/carrot.jpeg";
-import lemon from "../../assets/images/lemon.jpeg";
-import milk_on_strawberry from "../../assets/images/milk_on_strawberry.jpeg";
-import pepper_in_cloud from "../../assets/images/pepper_in_cloud.jpeg";
-import pepper from "../../assets/images/pepper.jpeg";
-import somefood from "../../assets/images/somefood.jpeg";
-import spaghetti from "../../assets/images/spaghetti.jpeg";
-import strawberry from "../../assets/images/strawberry.jpeg";
-import veggies from "../../assets/images/veggies.jpeg";
-
+import foodgif from "../../assets/images/Foodeaselogo.gif";
+import { Link } from "react-router-dom";
 
 function HomePageImages() {
   return (
-    <section className="food"> 
-    <div className="food__container">
-      <img className="food__image" src={broccoli} alt="broccoli" />
-      <img className="food__image" src={carrot} alt="carrot" />
-      <img className="food__image" src={lemon} alt="lemon" />
-      <img className="food__image" src={milk_on_strawberry} alt="milk_on_strawberry" />
-      <img className="food__image" src={pepper_in_cloud} alt="pepper_in_cloud" />
-      <img className="food__image" src={pepper} alt="pepper" />
-      <img className="food__image" src={somefood} alt="somefood" />
-      <img className="food__image" src={spaghetti} alt="spaghetti" />
-      <img className="food__image" src={strawberry} alt="strawberry" />
-      <img className="food__image" src={veggies} alt="veggies" />
-    </div>
+    <section className="homepage">
+      <img className="homepage__image" src={foodgif} alt="" />
+      <Link to="/"><h2 className="homepage__text">Get started</h2></Link>
+      
     </section>
   );
 }
