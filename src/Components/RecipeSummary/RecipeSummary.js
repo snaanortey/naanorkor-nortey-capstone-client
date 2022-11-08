3f25a8722d54bae9a5f324a4ff65fc92968c43f7
Naa Norkor <nortey.sarah@gmail.com>
2022-11-08 17:34:27 +0000

    styles getRecipe page

--- src/Components/RecipeSummary/RecipeSummary.js
+++ src/Components/RecipeSummary/RecipeSummary.js
@@ -1,58 +1,23 @@
 import React from "react";
 import { useState } from "react";
 import "./RecipeSummary.scss";
-import axios from "axios";
-import { useEffect } from "react";
+import veggies from "../../assets/images/veggies_icon.png";
+import clock from "../../assets/images/clock.png";
 
 export default function RecipeSummary(props) {
-  const [imageUrl, setImageUrl] = useState(null);
-
-  // useEffect(async () => {
-  //   const {
-  //     data: { image },
-  //   } = await axios.get("https://foodish-api.herokuapp.com/api/");
-
-  //   setImageUrl(image);
-  // },[]);
 
   return (
-    <div class="center">
-      <div class="property-card">
-        <div class="property-image">
-          {/* <img src="https://cdn.photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg" /> */}
+    <div class="recipeSummary">
+      <div class="recipeSummary__card">
+        <div class="recipeSummary__image">
           <img src={props.recipe.imageUrl}/>
-          <div class="property-image-title">
-            <h5>{props.recipe.title}</h5>
+        </div>
+        <div class="recipeSummary__description">
+          <h5 class="recipeSummary__title"> {props.recipe.title} </h5>
+          <div class="recipeSummary__details"> 
+          <div class="recipeSummary__details-div"> <img class="recipeSummary__details-image" src={veggies} alt="veggies" /><p>veggie</p></div> 
+          <div class="recipeSummary__details-div"><img class="recipeSummary__details-image" src={clock} alt="clock" /><p>30 min</p> </div>
           </div>
-        </div>
-        <div class="property-description">
-          <h5> {props.recipe.title} </h5>
-          <p>{props.recipe.instructions.substring(0, 50)}</p>
-        </div>
-      </div>
-    </div>
-  );
-
-  return (
-    <div className="recipeSummary">
-      <div className="recipeSummary__imagediv">
-        {/* <img
-          className="recipeSummary__image"
-          src={props.randomImage.image}
-          alt="random food image"
-        /> */}
-      </div>
-      <div className="recipeSummary__wrapTwo">
-        <div className="recipeSummary__contianer">
-          <h3 className="recipeSummary__title">{props.recipe.title}</h3>
-        </div>
-        <div className="recipeSummary__instructions">
-          <h3 className="recipeSummary__instructions-heading">
-            Cooking Instructions:
-          </h3>
-          <p className="recipeSummary__instructions-description">
-            {props.recipe.instructions.substring(0, 50)}...
-          </p>
         </div>
       </div>
     </div>
